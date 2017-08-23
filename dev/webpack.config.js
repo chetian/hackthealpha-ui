ce1c9ed5719866065c2a076432dc117069a3afac
dchetian <dan_chetian@intuit.com>
2017-08-22 20:29:02 -0700

    revert

--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   watch: true,
   devServer: {
     publicPath: "/",
-    contentBase: './build',
+    contentBase: './public',
     //   port: 9001,
     compress: true,
     hot: true,
